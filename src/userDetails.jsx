958b95c96348ae07b0a8c6222b2cb9f6f5337ea9
Neha Yadav <ny663922@gmail.com>
2025-07-25 16:27:39 +0530

    dyamic routing

--- src/userDetails.jsx
+++ src/userDetails.jsx
@@ -1,11 +1,14 @@
 
 import { Link } from "react-router";
+import { useParams } from "react-router";
 function UsersDetails() {
-
+    const paramData = useParams();
 
     return (
         <div style={{ marginLeft: 10 }}>
             <h2>UserDetails list page</h2>
+            <h3>user id is:{paramData.id}</h3>
+            <h3><Link to='/users'>back</Link></h3>
 
         </div>
     )
