103b74134142ad382f8a2357b477c52688b550e5
Neha Yadav <ny663922@gmail.com>
2025-08-03 01:12:23 +0530

    learning about put method in api

--- src/api/userEdit.jsx
+++ src/api/userEdit.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import { useEffect, useState } from "react";
 
 function UserEdit() {
@@ -6,12 +6,14 @@ function UserEdit() {
     const [Id, setId] = useState('');
     const [name, setName] = useState('');
     const [age, setAge] = useState('');
+    const url = "http://localhost:5000/users/" + id;
+    const navigate = useNavigate();
 
     useEffect(() => {
         updateUser()
     }, [])
     const updateUser = async () => {
-        const url = "http://localhost:5000/users/" + id;
+
         let response = await fetch(url)
         response = await response.json();
         setId(response.id);
@@ -19,17 +21,33 @@ function UserEdit() {
         setAge(response.age);
         console.log(response);
     }
+
+    const updateData = async () => {
+        console.log(id, name, age);
+        let response = await fetch(url, {
+            method: 'PUT',
+            body: JSON.stringify({ id: Id, name, age })
+        })
+        response = await response.json();
+        console.log(response);
+        if (response) {
+            alert("data updated succesful")
+            navigate("/")
+        }
+
+    }
+
     return (
-        // <div className="form-container">
-        //     <h1>User Edit Page</h1>
-        //     <input type="text" value={id}
-        //         placeholder="Enter your id" /><br /><br />
-        //     <input type="text" value={name}
-        //         placeholder="Enter your name" /><br /><br />
-        //     <input type="text" value={age}
-        //         placeholder="Enter your age" /><br /><br />
-        //     <button>Update User</button>
-        // </div>
+        <div className="form-container">
+            <h1>User Edit Page</h1>
+            <input type="text" value={Id} onChange={(event) => setId(event.target.value)}
+                placeholder="Enter your id" /><br /><br />
+            <input type="text" value={name} onChange={(event) => setName(event.target.value)}
+                placeholder="Enter your name" /><br /><br />
+            <input type="text" value={age} onChange={(event) => setAge(event.target.value)}
+                placeholder="Enter your age" /><br /><br />
+            <button onClick={updateData}>Update User</button>
+        </div>
     )
 }
 export default UserEdit;
